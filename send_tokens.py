fb61c7a74fe18b90b0d304d8f09dd9bdd975a251
lechu91 <51946227+lechu91@users.noreply.github.com>
2022-07-25 23:21:41 -0700

    Update send_tokens.py

--- send_tokens.py
+++ send_tokens.py
@@ -30,7 +30,7 @@ def send_tokens( receiver_pk, tx_amount ):
    
     txn = transaction.PaymentTxn(pk, tx_fee, first_valid_round, last_valid_round, gen_hash, receiver_pk, tx_amount)
     signed_txn = txn.sign(sk)
-    txid = txn.send_transaction(signed_txn)
+    txid = signed_txn.send_transaction(signed_txn)
 
     return sender_pk, txid
 
